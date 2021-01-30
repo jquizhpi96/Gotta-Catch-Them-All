0f83009daf2d54c57f4140ed6659a872a613e09c
Jason Quizhpi <jasonquizhpi@Jasons-MacBook-Pro-2.local>
2021-01-30 00:03:21 -0500

    new axios

--- app.js
+++ app.js
@@ -1,92 +1,143 @@
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {
  // fetching api data.
-//  async function fetchOrigPokes(pokemonName) {
-//   const allPokemon = `https://pokeapi.co/api/v2/pokemon/${id}`
+ // const name = await axios.get(`https://cors-anywhere.herokuapp.com/${url.name}`)
+
+async function fetchPokemon() {
+  for (let i = 1; i <= 150; i++) {
+    try {
+      const allPokemon = `https://pokeapi.co/api/v2/pokemon/${i}`
+      const response = await axios.get(allPokemon)
+      //console.log(response.data)
+      let data = response.data.sprites.front_default
+      console.log(data)
+    
+    } catch (error) {
+    
+    }
+  }
+}   
+fetchPokemon()
+
+function createPokemon() {
+  let appendingPokemon = Math.round(Math.random() * 151)
+  console.log(appendingPokemon)
+}
+createPokemon(data)
+// const showPokeImage = ((pokemon) => {
+//   const pokeImage = `
+//   <img src='${pokemon.sprites.front_default}' alt ='Pokemon Image' class='image'>
+// `
+// })
+// console.log(showPokeImage)
+  // //appending elements to the DOM.
  
+  //  function createPokemons() {
+  //   let firstPokemon = Math.round(Math.random() * 750)
+  //   let secondPokemon = Math.round(Math.random() * 750)
+  //   let thirdPokemon = Math.round(Math.random() * 750)
+  //   let fourthPokemon = Math.round(Math.random() * 750)
+  //   let fifthPokemon = Math.round(Math.random() * 750)
+  //   let sixthPokemon = Math.round(Math.random() * 750)
+  //   let sevenPokemon = Math.round(Math.random() * 750)
+  //   let eighthPokemon = Math.round(Math.random() * 750)
+  //   let ninethPokemon = Math.round(Math.random() * 750)
+  //   let tenthPokemon = Math.round(Math.random() * 750)
+  //   let eleventhPokemon = Math.round(Math.random() * 750)
+  //   let twelvethPokemon = Math.round(Math.random() * 750)
+    // fetchPokemon(firstPokemon)
+    // fetchPokemon(secondPokemon)
+    // fetchPokemon(thirdPokemon)
+    // fetchPokemon(fourthPokemon)
+    // fetchPokemon(fifthPokemon)
+    // fetchPokemon(sixthPokemon)
+    // fetchPokemon(sevenPokemon)
+    // fetchPokemon(eighthPokemon)
+    // fetchPokemon(ninethPokemon)
+    // fetchPokemon(tenthPokemon)
+    // fetchPokemon(eleventhPokemon)
+    // fetchPokemon(twelvethPokemon)
+  //}
+//   createPokemons()
+//  function showPokemon() {
+   
+//   let image = document.createElement('img')
+//      console.log(image)
+//    image.setAttribute("src", pokemon.sprites.front_default)
 
-//   try {
-//     const response = await axios.get(`${allPokemon}${pokemonName}`)
-//      console.log(response.data)
+//    }
 
-//     let data = response.data
-//     showPokeData(data)
-//     //console.log(showPokeData)
 
-//     const descriptions = await axios.get(`${pokeDescr}${pokemonName}`)
-//     let descriptionsData = descriptions.data
-//     showPokeDescription(descriptionsData)
 
-//   } catch (error) {
-//    // alert(`Ooops! It looks like you didn't enter anything or what you entered was spelled incorrectly, try again!`)
+
+
+
+
+
+
+
+
+// //making the pokemon to jump in CSS.
+  
+//   const scoreBoard = document.querySelector('.score')
+//   const grass = document.querySelectorAll(".grass")
+//   const pokemon = document.querySelectorAll(".pokemon")
+//   const pokemonImage = document.querySelectorAll('.image')
+//   let score = 0
+//   let timeLimit = 2000;
+//   let timeUp = false;
+// let countdown
+// //removing pokemon when clicked 
+  
+//   function removePokemon() {
+//     console.log(pokemon)
+//   }
+//  removePokemon()
+//  pokemon.forEach(pokemon => pokemon.addEventListener('click', removePokemon))
+  
+ // score.addEventListener('click', score ++)
+  // img.addEventListener('click', removePokemon())
+
+  //recording score
+  // if user clicks on pokemon, they a point, otherwise, no points.
+  
+//start Game
+  
+  // function startGame(){
+  //   let countdown = timeLimit / 1000
+  //   scoreBoard.textContent = 0;
+  //   scoreBoard.style.display = `block`;
+  //   countdownBoard.textContent = countdown;
+  //   timeUp = false;
+  //   score = 0;
+  //   setTimeout(() => {
+  //     timeUp = true 
+  //   }, timeLimit);
+
+    
+  // }
+  
+ 
+// function checkForWinner() {
+//   if (document.querySelectorAll('.pokemon').length === 0) 
+//   // scoreBoard === 12;
+//   {
+//    alert('You win!')
 //   }
 // }
-//fetchOrigPokes('id')
-
-  function fetchPokemon(id, num) {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-      .then(res => res.json())
-      .then(pokemon => {
-        showPokemon(pokemon, num)
-        
-      });
-  } //appending elements to the DOM.
+//   checkForWinner()
+  
+  
  
-  function createPokemons() {
-    let firstPokemon = Math.round(Math.random() * 750)
-    let secondPokemon = Math.round(Math.random() * 750)
-    let thirdPokemon = Math.round(Math.random() * 750)
-    let fourthPokemon = Math.round(Math.random() * 750)
-    let fifthPokemon = Math.round(Math.random() * 750)
-    let sixthPokemon = Math.round(Math.random() * 750)
-    let sevenPokemon = Math.round(Math.random() * 750)
-    let eighthPokemon = Math.round(Math.random() * 750)
-    let ninethPokemon = Math.round(Math.random() * 750)
-    let tenthPokemon = Math.round(Math.random() * 750)
-    let eleventhPokemon = Math.round(Math.random() * 750)
-    let twelvethPokemon = Math.round(Math.random() * 750)
-    fetchPokemon(firstPokemon, 1)
-    fetchPokemon(secondPokemon, 2)
-    fetchPokemon(thirdPokemon, 3)
-    fetchPokemon(fourthPokemon, 4)
-    fetchPokemon(fifthPokemon, 5)
-    fetchPokemon(sixthPokemon, 6)
-    fetchPokemon(sevenPokemon, 7)
-    fetchPokemon(eighthPokemon, 8)
-    fetchPokemon(ninethPokemon, 9)
-    fetchPokemon(tenthPokemon, 10)
-    fetchPokemon(eleventhPokemon, 11)
-    fetchPokemon(twelvethPokemon, 12)
-  }
-  createPokemons()
-  function showPokemon(pokemon, num) {
-    let item = document.querySelector(`#pokemon-${num}`)
-    let image = item.getElementsByTagName('img')[0]
-    image.setAttribute("src", pokemon.sprites.front_default)
-  }
+  
+// })
 
 
-  //making the pokemon to jump
 
-  
-  const start = document.querySelector('#button')
-  const scoreBoard = document.querySelector('#score')
-  const grass = document.querySelectorAll(".grass")
-  const img = document.querySelectorAll(".pokemon")
-  
-
-  
-  function randomTime(min, max) {
-    return Math.round(Math.random() * (max - min) + min)
-   
-   
- }
- //console.log(randomTime(200, 1000))
-  
-  // let lastImage;
-  let timeLimit = 20000;
-  let score = 0;
-  let countDown;
-  const pokemons = document.querySelectorAll(".image")
+// let lastImage;
+  // let timeLimit = 20000;
+  // let score = 0;
+  // let countDown;
+  // const pokemons = document.querySelectorAll(".image")
 
   
 
@@ -123,25 +174,3 @@ document.addEventListener('DOMContentLoaded', () => {
   //   }, 2000);
   // }
   // jump()
-  
-  // //removing pokemon
-
-  function removePokemon() {
-    let pokemon = document.querySelectorAll('.image')
-    console.log(pokemon)
-     pokemon.remove()
-  }
-  removePokemon()
-  
-
-// function checkForWinner() {
-//   if (document.querySelectorAll('.pokemon').length === 0) {
-//     console.log('You win!')
-//   }
-// }
-//   checkForWinner()
- 
-  
-})
-
-
